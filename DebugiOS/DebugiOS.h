80c37688dcf652126e9fca19297d51062ca59f84
BotreeMac <dipang.sheth@botreetechnologies.com>
2017-11-30 11:40:36 +0530

    Initial Commit

---
+++ DebugiOS/DebugiOS.h
@@ -0,0 +1,19 @@
+//
+//  DebugiOS.h
+//  DebugiOS
+//
+//  Created by BotreeMac on 30/11/17.
+//  Copyright © 2017 Botree Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DebugiOS.
+FOUNDATION_EXPORT double DebugiOSVersionNumber;
+
+//! Project version string for DebugiOS.
+FOUNDATION_EXPORT const unsigned char DebugiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DebugiOS/PublicHeader.h>
+
+
